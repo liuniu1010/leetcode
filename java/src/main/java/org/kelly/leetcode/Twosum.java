07a564b8dd5b3f29954bf695dde481b0b9b63463
Liu, Niu <liuniu@tsinghua.org.cn>
2015-01-14 15:47:35 +0800

    twosum exam of leetcode

---
+++ java/src/main/java/org/kelly/leetcode/Twosum.java
@@ -0,0 +1,74 @@
+package org.kelly.leetcode;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.kelly.leetcode.exception.InvalidInputException;
+
+/**
+ * Given an array of integers, find two numbers such that they add up to a specific target number.
+ * The function twoSum should return indices of the two numbers such that they add up to the target, 
+ * where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are zero-based.
+ * You may assume that each input would have exactly one solution.
+ * Input: numbers={2, 7, 11, 15}, target=9
+ * Output: index1=0, index2=1
+ *
+ */
+public class Twosum {
+    /**
+     * search the two indexes
+     * @param input
+     * @param target
+     * @return          if the two indexes were found out, return the related Result instance,
+     *                  else return null
+     */
+    public static Result searchTwosum(List<Integer> input, Integer target) {
+        assertInput(input, target);
+        
+        Map<Integer, Integer> inputCache = new HashMap<Integer, Integer>();
+        
+        /***
+         * first loop through the input, record the value and index into cache
+         * to make sure the overall loop times is O(input.size), not O(input.size^2)
+         */
+        for(int i = 0;i < input.size();i++) {
+            inputCache.put(input.get(i), i);
+        }
+        
+        for(int i = 0;i < input.size();i++) {
+            int currentValue = input.get(i);
+            int searchValue = target - currentValue;
+            if(inputCache.containsKey(searchValue)) {
+                Result result = new Result();
+                result.index1 = i;
+                result.index2 = inputCache.get(searchValue);
+                return result;
+            }
+        }
+        
+        return null;
+    }
+    
+    private static void assertInput(List<Integer> input, Integer target) {
+        if(input == null) {
+            throw new InvalidInputException("input should not be null!");
+        }
+        else if(input.size() < 2) {
+            throw new InvalidInputException("input length should be greater or equal than 2");
+        }
+        else if(target == null) {
+            throw new InvalidInputException("target should not be null!");
+        }
+    }
+}
+
+class Result {
+    public Result() {
+        index1 = 0;
+        index2 = 0;
+    }
+    
+    public int index1;
+    public int index2;
+}
---
+++ java/src/test/java/org/kelly/leetcode/TwosumTest.java
@@ -0,0 +1,52 @@
+package org.kelly.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class TwosumTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public TwosumTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( TwosumTest.class );
+    }
+
+    public void testTwosum()
+    {
+        List<Integer> input = new ArrayList<Integer>();
+        input.add(8);
+        input.add(7);
+        input.add(2);
+        input.add(15);
+        
+        Integer target = 23;
+        
+        Result result = Twosum.searchTwosum(input, target);
+        if(result != null) {
+            System.out.println("index1 = " + result.index1);
+            System.out.println("index2 = " + result.index2);
+        } else {
+            System.out.println("cannot find the two indexes!");
+        }
+    }
+}
